cba1d6965923ec9bc34075288cdadb4e2d8a3fdb
anujsyadav <anujsyadav@gmail.com>
2025-03-31 11:23:50 +0530

    Reverted adding functionality

--- index.py
+++ index.py
@@ -5,3 +5,7 @@
 num1 = int(input("Enter your first number: "))
 num2 = int(input("Enter your first number: "))
 
+#Step 2 Adding functionality through functions 
+
+def Addition(num1,num2):
+    return num1+num2
