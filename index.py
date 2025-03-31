44f2e05cbe5cf47fcb68895de55b9afdef8f6552
anujsyadav <anujsyadav@gmail.com>
2025-03-31 10:59:02 +0530

    Added two inputs from user

---
+++ index.py
@@ -0,0 +1,7 @@
+# Calculator Functionality
+
+#Step 1 Take input two variables for functionality working
+
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your first number: "))
+
