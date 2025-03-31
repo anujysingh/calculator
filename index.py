2b3086a628806bd965420768a0e60124c4e2a32b
Anuj Singh Yadav <114822651+anujysingh@users.noreply.github.com>
2025-03-31 23:31:53 +0530

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,7 @@
 
 #Step 1 Take input two variables for functionality working
 
-num1 = int(input("Enter your first number: "))
+num3 = int(input("Enter your first number: "))
 num2 = int(input("Enter your first number: "))
 
 #Substraction functionality 
